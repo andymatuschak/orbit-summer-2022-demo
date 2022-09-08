73b54b897e4374a5cb8f86b21fb8ba73e48af58c
Hammad Bashir <hammadb@berkeley.edu>
2022-09-08 15:30:02 -0400

    Update stories with states

--- src/components/PromptBox.stories.jsx
+++ src/components/PromptBox.stories.jsx
@@ -8,7 +8,7 @@ export default {
 
 const Template = (args) => <PromptBox {...args} />;
 
-const samplePrompt = { 
+const unsavedPrompt = { 
     content: {
         front: "What is a test?",
         back: "A procedure intended to establish the quality, performance, or reliability of something, especially before it is taken into widespread use."
@@ -18,8 +18,25 @@ const samplePrompt = {
   isSaved: false
 };
 
-export const Primary = Template.bind({});
-Primary.args = {
-    prompt: samplePrompt,
+const savedPrompt = { 
+    content: {
+        front: "What is a test?",
+        back: "A procedure intended to establish the quality, performance, or reliability of something, especially before it is taken into widespread use."
+    },
+  selectors: [{type: PromptSelectorType.TextPositionSelector, start:0, end: 100}],
+  isByAuthor: true,
+  isSaved: true
+};
+
+export const Unsaved = Template.bind({});
+Unsaved.args = {
+    prompt: unsavedPrompt,
+    savePrompt: () => null
+};
+
+
+export const Saved = Template.bind({});
+Saved.args = {
+    prompt: savedPrompt,
     savePrompt: () => null
 };
