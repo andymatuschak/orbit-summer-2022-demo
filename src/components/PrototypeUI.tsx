9ba7b9ad26aa03e11a50a8d9e6cc77d76de2266b
Hammad Bashir <hammadb@berkeley.edu>
2022-09-12 11:57:37 -0700

    Change how new prompts are unmarked to be correct react. Add shortcut handling

--- src/components/PrototypeUI.tsx
+++ src/components/PrototypeUI.tsx
@@ -77,6 +77,7 @@ export default function PrototypeUI({
                   }
                 },
                 shortcutKey: "N",
+                isEnabled: selectionPosition ? true : false,
               },
             ]}
           />
