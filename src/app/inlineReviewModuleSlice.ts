247dca267dcf97262b35c74e304528d9d2e400f3
Andy Matuschak <andy@andymatuschak.org>
2022-09-20 14:08:10 -0700

    Closes andymatuschak/orbit-summer-2022-demo#50 Add inline review areas to IMS

--- src/app/inlineReviewModuleSlice.ts
+++ src/app/inlineReviewModuleSlice.ts
@@ -1,4 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  PromptLocation,
+  resolvePromptLocations,
+} from "../util/resolvePromptLocations";
 import { loadPrompts, PromptsState } from "./promptSlice";
 
 export interface Rect {
@@ -29,8 +33,24 @@ const inlineReviewModuleSlice = createSlice({
   },
   extraReducers(builder) {
     // We scan the page for review modules once we've loaded the prompt data for the page.
-    builder.addCase(loadPrompts.fulfilled, (_state, action) => {
-      return indexReviewModules(action.payload);
+    builder.addCase(loadPrompts.fulfilled, (state, action) => {
+      const prompts = action.payload;
+      const modules = indexReviewModules(prompts);
+      // HACK: not really kosher to cause an async side-effect from a reducer like this, but eh.
+      (async () => {
+        const promptLocations = await resolvePromptLocations(prompts);
+        const protoReviewAreas =
+          document.getElementsByClassName("orbit-reviewarea");
+        const populatedPromptIDs = new Set<string>();
+        for (const protoReviewArea of protoReviewAreas) {
+          populateReviewArea(
+            protoReviewArea,
+            prompts,
+            promptLocations,
+            populatedPromptIDs,
+          );
+        }
+      })();
     });
     // TODO: implement extra reducers to update inline review module state when prompt contents are edited
   },
@@ -90,4 +110,54 @@ function getFrame(element: HTMLElement): Rect {
     width,
     height,
   };
+}
+
+function populateReviewArea(
+  element: Element,
+  prompts: PromptsState,
+  promptLocations: { [id: string]: PromptLocation },
+  populatedPromptIDs: Set<string>,
+): void {
+  const populatedEntries = Object.entries(prompts).filter(([id, prompt]) => {
+    if (populatedPromptIDs.has(id)) return false;
+    const range = promptLocations[id].range;
+    return rangeCompareNode(range, element) === 1;
+  });
+
+  if (populatedEntries.length === 0) {
+    throw new Error("No prompts seem to belong to review area!");
+  }
+
+  const reviewAreaElement = document.createElement("orbit-reviewarea");
+  reviewAreaElement.setAttribute("color", "brown");
+  for (const [id, prompt] of populatedEntries) {
+    populatedPromptIDs.add(id);
+    const promptElement = document.createElement("orbit-prompt");
+    promptElement.setAttribute("question", prompt.content.front);
+    promptElement.setAttribute("answer", prompt.content.back);
+    promptElement.id = id;
+    // TODO: handle attachments
+    reviewAreaElement.appendChild(promptElement);
+  }
+  element.after(reviewAreaElement);
+}
+
+// From https://developer.mozilla.org/en-US/docs/Web/API/Range/compareNode
+function rangeCompareNode(range: Range, node: Node) {
+  const nodeRange = document.createRange();
+  try {
+    nodeRange.selectNode(node);
+  } catch (e) {
+    nodeRange.selectNodeContents(node);
+  }
+  const nodeIsBefore =
+    range.compareBoundaryPoints(Range.START_TO_START, nodeRange) === 1;
+  const nodeIsAfter =
+    range.compareBoundaryPoints(Range.END_TO_END, nodeRange) === -1;
+
+  if (nodeIsBefore && !nodeIsAfter) return 0;
+  if (!nodeIsBefore && nodeIsAfter) return 1;
+  if (nodeIsBefore && nodeIsAfter) return 2;
+
+  return 3;
 }
